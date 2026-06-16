06a30968f4f04ac8ece20fe1c9ca5a722fd6abb7
Marcus Filippi <marcusfilippi5@gmail.com>
2026-06-16 17:21:20 -0300

    finaleira lista 3

---
+++ lista 3/atividade4lista3.py
@@ -0,0 +1,59 @@
+#4 - Codifique um programa que funcionará como um cadastro de placas de automóveis de
+#um estacionamento (para até 15 automóveis). O cadastro deve ser realizado em uma
+#lista. Seu programa deve ter um menu com a seguinte estrutura:
+#1 – Cadastrar
+#2 - Excluir
+#3 - Listar
+#0 - Sair
+#A opção Cadastrar deve verificar se há espaço disponível na lista para o cadastro. Se
+#houver, deve proceder o cadastro. Se não, deve informar o usuário que não há espaço
+#disponível. A opção Excluir deve perguntar ao usuário qual placa deve ser excluída (pelo
+#nome da placa) e informar se houve sucesso ou falha. Já a opção listar deve
+#simplesmente listar todas as placas cadastradas. Dica: utilize um valor padrão para definir
+#um espaço vago na lista.
+
+placas = [0] * 15
+contplacas = 0
+x =0
+i =0
+p = 0
+codigoaniquilado = 0
+
+
+while True:
+    print ("Menu:")
+    print ("----")
+    print ("1 – Cadastrar")
+    print ("2 – Excluir")
+    print ("3 – Listar")
+    print ("0 – Sair")
+
+    opcao = int(input("Opção: "))
+
+    if opcao == 0:
+         break 
+    elif opcao == 1:
+         if contplacas <= 14:
+             i = int(input("Digite seu código:"))
+             if i != 0:
+              while True:
+                  if x == 0:
+                      placas[0] == 0
+                      contplacas = contplacas+1
+                      break
+                  x = x + 1
+             else:
+                 print("Não é possível adicionar um código igual a 0.")
+         else:
+             print("15 códigos já armazenados. Você chegou no limite!")
+
+    elif opcao == 2:
+           codigoaniquilado = int(input("Digite o código a ser excluído:"))
+
+           while p <=14:
+               if placas[p] == codigoaniquilado:
+                   placas [p] = 0
+                   break
+               p = p +1
+            
+                
